3a26a905ce861e3512efdaaa23bbf2af9e906efc
starrysky <starrysky8888@qq.com>
2023-03-03 10:49:47 +0800

    完善脚本逻辑

--- src/scripts/aide.py
+++ src/scripts/aide.py
@@ -26,17 +26,34 @@ class Aide(object):
     def migrate_db(self):
         """数据库迁移"""
 
+        logger.info(">>> 开始迁移数据库")
+
         os.chdir(BASE_DIR)
         try:
             os.system(f"{sys.executable} manage.py makemigrations")
             os.system(f"{sys.executable} manage.py migrate --fake-initial")
         except Exception as e:
-            logger.info(f"数据库迁移失败: {e}")
-            sys.exit(1)
+            logger.error(f"!!! 数据库迁移失败: {e}")
+            self.exit(1)
+
+        logger.info(">>> 迁移数据库完成")
+
+    def exit(self, code=0):
+        logger.info("*** 助手程序执行结束 ***")
+        sys.exit(code)
 
     def exec(self):
         """开始执行"""
-        pass
+        logger.info("*** 助手程序开始执行 ***")
+
+        store_true_keys = ["migrate_db"]
+        for k in store_true_keys:
+            if getattr(self.args, k):
+                # 执行具体方法
+                getattr(self, k)()
+        
+        
+        self.exit()
 
 
 if __name__ == "__main__":
@@ -48,5 +65,4 @@ if __name__ == "__main__":
         sys.exit(2)
 
     args = parse.parse_args()
-    print(args)
     Aide(args=args).exec()
